24828c7f39cdc379d401b337b8f6ae16076fd37b
orlagh-sweeney <orlagh16@gmail.com>
2023-06-07 10:24:05 +0000

    Deployment commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn thebigreview.wsgi
--- thebigreview/settings.py
+++ thebigreview/settings.py
@@ -18,6 +18,7 @@ if os.path.isfile('env.py'):
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
@@ -29,7 +30,11 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-orlaghsween-thebigrevie-b1x9j4vc62f.ws-eu98.gitpod.io']
+ALLOWED_HOSTS = [
+    '8000-orlaghsween-thebigrevie-b1x9j4vc62f.ws-eu98.gitpod.io',
+    'the-big-review.herokuapp.com',
+    'localhost',
+]
 
 
 # Application definition
@@ -61,7 +66,7 @@ ROOT_URLCONF = 'thebigreview.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -130,7 +135,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATICFILES_DIRS = [os.environ.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
