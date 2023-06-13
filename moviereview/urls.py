146e773cce097cf0fcd672a4a40f693c95addc5c
orlagh-sweeney <orlagh16@gmail.com>
2023-06-13 21:33:47 +0000

    Create view and templte for movie search results

--- moviereview/urls.py
+++ moviereview/urls.py
@@ -1,8 +1,10 @@
 from . import views
+from .views import SearchResultsView
 from django.urls import path
 from django.views.generic import TemplateView
 
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html')),
+    path('search/', SearchResultsView.as_view(), name='search')
 ]
--- moviereview/views.py
+++ moviereview/views.py
@@ -1,9 +1,39 @@
-from django.shortcuts import render
 import requests
+import os
+from django.http import JsonResponse
+from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from django.views.generic import TemplateView
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import Review, ReviewLikes
+
+
+api_key = os.environ.get('MY_API_KEY')
+
+headers = {
+    "accept": "application/json",
+    "Authorization": f"Bearer {api_key}"
+}
 
 
 class HomeView(TemplateView):
     template_name = 'index.html'
+
+
+class SearchResultsView(View):
+    template_name = 'search_results.html'
+    paginate_by = 6
+    print('test 1')
+
+    def get(self, request, *args, **kwargs):
+        query = request.GET.get('q')
+        url = f"https://api.themoviedb.org/3/search/movie?query={query}&api_key={api_key}&include_adult=false&language=en-US&page=1"
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            data = response.json
+            print(response.text)
+
+        return render(request, 'search_results.html', {
+            "data": data,
+        })
