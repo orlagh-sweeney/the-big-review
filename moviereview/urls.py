1828d8260aefe85b27ee8130acc46dc73b62f589
orlagh-sweeney <orlagh16@gmail.com>
2023-06-14 14:30:07 +0000

    Fix HomeView path in urls.py

--- moviereview/urls.py
+++ moviereview/urls.py
@@ -1,11 +1,11 @@
 from . import views
-from .views import SearchResultsView, MovieDetailView
+from .views import SearchResultsView, MovieDetailView, HomeView
 from django.urls import path
 from django.views.generic import TemplateView
 
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html')),
+    path('', HomeView.as_view(), name='home'),
     path('search/', SearchResultsView.as_view(), name='search'),
     path('search/movie/<int:movie_id>/', MovieDetailView.as_view(), name='moviedetail')
 ]
