9dd80b463903cc323715664e6e1e9dcfdc7b0e08
orlagh-sweeney <orlagh16@gmail.com>
2023-06-18 10:54:55 +0000

    Update urls for addreview and editreview

--- moviereview/urls.py
+++ moviereview/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('search/', SearchResultsView.as_view(), name='search'),
     path('search/movie/<int:movie_id>/', MovieDetailView.as_view(), name='moviedetail'),
-    path('search/movie/<int:movie_id>/movie/addreview/', AddReviewView.as_view(), name='addreview'),
-    path('edit/<review_id>', EditReviewView.as_view(), name='edit'),
+    path('search/movie/<int:movie_id>/addreview/', AddReviewView.as_view(), name='addreview'),
+    path('search/movie/<int:movie_id>/edit/<review_id>', EditReviewView.as_view(), name='edit'),
 ]
